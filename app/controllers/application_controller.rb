fad022a75ab3ec483c640a26d27969c55253790d
Tomas Landovsky <435847+landovsky@users.noreply.github.com>
2019-03-15 12:46:08 +0100

    Load resource in app controller for show / update / destroy actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  before_action :load_resource, only: [:show, :update, :destroy]
+
+  def load_resource
+    @resource = controller_name.classify.constantize.find(params[:id])
+  end
 end
